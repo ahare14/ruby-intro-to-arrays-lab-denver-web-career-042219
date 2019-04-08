1cd2f6dfa290dd2c8e8d7ba837d93c9cbd7f977e
Alexander Hare <alexanderhare14@gmail.com>
2019-04-08 15:23:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,9 +2,6 @@ def instantiate_new_array
   []
 end
 
-def array_with_two_elements(array, element1, element2)
-  new_array = []
-  new_array << element1
-  new_array << element2
-  new_array
+def array_with_two_elements
+  ["thing1", "thing2"]
 end
